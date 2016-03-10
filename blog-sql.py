9049f99629416561acc772299138176dc430475b
znss1989 <znss1989@gmail.com>
2016-03-10 09:42:00 +0800

    set database up

---
+++ blog-sql.py
@@ -0,0 +1,11 @@
+# Database for blog app
+import sqlite3
+
+with sqlite3.connect("blog.db") as connection:
+    c = connection.cursor()
+    # Create table posts
+    c.execute("""CREATE TABLE posts
+    (title TEXT, post TEXT)""")
+    # Insert content
+    c.execute('INSERT INTO posts VALUES("My first blog", "Hello world! This is my first blog and a \
+        starting point to explore the world.")')
